d18b0c40857179843ae71ce8a6682acdade09134
Glider <2929799602@qq.com>
2023-08-01 23:22:32 +0800

    feat: popup 和 contentScript  通信

--- src/content.js
+++ src/content.js
@@ -2,7 +2,7 @@ const getAllIframeSrc = () => {
   const iframes = document.querySelectorAll("iframe");
   const iframeSrc = [];
   iframes.forEach((iframe) => {
-    iframeSrc.push(iframe.src);
+    iframeSrc.push(iframe);
   });
   return iframeSrc;
 };
@@ -10,20 +10,8 @@ const init = () => {
   console.log(getAllIframeSrc());
 };
 
-init();
+// init();
 
-var port = chrome.runtime.connect();
-window.addEventListener(
-  "message",
-  (event) => {
-    // We only accept messages from ourselves
-    if (event.source !== window) {
-      return;
-    }
-    if (event.data.type && event.data.type === "FROM_PAGE") {
-      console.log(port, "this port");
-      //   port.postMessage(event.data.text, event.origin);
-    }
-  },
-  false
-);
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  sendResponse(getAllIframeSrc());
+});
