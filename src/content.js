bf76e06303c39128f2f15ba2ec0cb8de75618ddd
Glider <2929799602@qq.com>
2023-08-02 16:28:57 +0800

    feat: 获取到页面的iframe src 以及对应的名称

--- src/content.js
+++ src/content.js
@@ -1,17 +1,26 @@
-const getAllIframeSrc = () => {
-  const iframes = document.querySelectorAll("iframe");
-  const iframeSrc = [];
-  iframes.forEach((iframe) => {
-    iframeSrc.push(iframe);
-  });
-  return iframeSrc;
-};
-const init = () => {
-  console.log(getAllIframeSrc());
-};
+const MATCH_PRE_HTTP = [
+  "http://101.37.228.6:88",
+  "http://101.37.228.6:86",
+  "http://101.37.228.6:96",
+];
 
-// init();
+const getAllIframeSrc = () => {
+  const liArr = document.querySelectorAll(".ma_taps > li");
+  const matchedIframes = [...liArr].filter((liDom) =>
+    MATCH_PRE_HTTP.some((matchItem) =>
+      liDom.getAttribute("tab-id").startsWith(matchItem)
+    )
+  );
+
+  return matchedIframes;
+};
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  sendResponse(getAllIframeSrc());
+  let infoArr = getAllIframeSrc().map((liDom) => {
+    return {
+      src: liDom.getAttribute("tab-id").replace(/^.*(?=\?)/, ""),
+      name: liDom.querySelector("span").textContent,
+    };
+  });
+  sendResponse(infoArr);
 });
--- src/popup.js
+++ src/popup.js
@@ -11,13 +11,46 @@ let handleEvent = () => {
         // 当前tab发送事件
         chrome.tabs.sendMessage(currentTabId, "", (res) => {
           iframeArr = res;
-
-          console.log(iframeArr, "iframeArr");
+          generateDom();
         });
       });
     },
     false
   );
+
+  document.querySelector("#container").addEventListener(
+    "click",
+    (e) => {
+      console.log(e.target);
+      if (e.target.tagName === "LI") {
+        let targetSrc = e.target.getAttribute("data-src");
+        copy(targetSrc);
+      }
+    },
+    false
+  );
 };
+
+const generateDom = () => {
+  let fragment = document.createDocumentFragment();
+
+  for (let i = 0; i < iframeArr.length; i++) {
+    let li = document.createElement("li");
+    li.textContent = iframeArr[i].name;
+    li.setAttribute("data-src", iframeArr[i].src);
+    fragment.appendChild(li);
+  }
+
+  document.querySelector("#container").appendChild(fragment);
+};
+
+function copy(text) {
+  let tmpTextarea = document.createElement("textarea");
+  tmpTextarea.value = text;
+  document.body.appendChild(tmpTextarea);
+  tmpTextarea.select();
+  document.execCommand("copy");
+  document.body.removeChild(tmpTextarea);
+}
 
 document.addEventListener("DOMContentLoaded", handleEvent);
