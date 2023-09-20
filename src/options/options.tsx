153b5bc0d2820b163492c11bf1b75e17106d9701
Glider <2929799602@qq.com>
2023-09-20 22:59:09 +0800

    feat: options 页面表格设计

--- src/options/options.tsx
+++ src/options/options.tsx
@@ -1,23 +1,74 @@
 import { createRoot } from 'react-dom/client'
 import React, { useState, useEffect } from 'react'
-import { Button, Table } from 'antd'
+import { Button, Table, Card, Form, Input, Row, Col } from 'antd'
+// TODO: 能否提取成为公共样式
+import '../popup/index.css'
 
 function AddServerUrlForm() {
-    return <>
-        <Button type="primary" >查询</Button>
-        <Button className="ml-4">新增</Button>
-    </>
+    return <Card >
+        <Form
+            name="searchAndAdd"
+            labelCol={{ span: 8 }}
+            wrapperCol={{ span: 16 }}>
+            
+            <Row>
+             <Col span={10}>
+                    <Form.Item label={'在线服务器地址'}>
+                        <Input></Input>
+                    </Form.Item>
+                    </Col>
+
+                <Col span={10}>
+                    <Form.Item label={'本地服务器地址'}>
+                        <Input></Input>
+                    </Form.Item>
+                </Col>
+
+                <Col span={4}>
+                    <Button className="ml-2">查询</Button>
+                    <Button className="ml-2">新增</Button>
+                </Col>
+                
+            </Row>
+
+        </Form>
+           
+        </Card>
+    
 }
+
+/**
+ * table 
+ *
+*/
+const columns = [
+    {
+        title: '线上url',
+        dataIndex: 'name',
+        key: 'name',
+        // render: (text) => <a>{text}</a>,
+      },
+      {
+        title: '本地url地址',
+        dataIndex: 'age',
+        key: 'age',
+      },
+      {
+        title: 'Address',
+        dataIndex: 'address',
+        key: 'address',
+      },
+]
 
 function ServerUrlTable() {
     return <></>
 }
 
 function App() {
-    return <>
+    return <div className="p-2">
         <AddServerUrlForm />
         <ServerUrlTable />
-    </>
+    </div>
 }
 
 const root = createRoot(document.getElementById('app'));
